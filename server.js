0c34d54a2a580aed9124157708c667396fb6988e
lspk129 <l.spukas@gmail.com>
2017-03-22 21:54:13 +0200

    Initial commit

--- server.js
+++ server.js
@@ -4,20 +4,25 @@ const path = require('path');
 const isValid = require('./valid');
 const generator = require('./generator')
 
+// url 'mongodb://<dbuser>:<dbpassword>@ds137100.mlab.com:37100/url' is set to enviroment variable
+// for not exposing user and password publicaly
 const mongoLabUrl = process.env.MONGOLAB_URI;
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// static server for file delivery
 app.use(express.static(path.join(__dirname, 'public')));
+
 // // Home page
 app.get('/', (req, res) => {
   console.log('Connected to server');
   res.sendFile(path.join(__dirname, 'public/index.html'));
-}); // end Home page
+});
 
-// URL shortening and DB insert function
+// route for passing users url
 app.get('/new/:url(*)', (req, res) => {
+  // connect to MongoLab database server
   mongo.connect(mongoLabUrl, (err, db) => {
     if (err) console.log('Error connecting to DB server: ' + err);
     console.log('Connected to server');
@@ -25,61 +30,73 @@ app.get('/new/:url(*)', (req, res) => {
     const params = req.params.url;
     const collection = db.collection('links');
 
-    const insertDoc = (db, callback) => {
-      // check if valid and: 1) generate id; 2) export to db; 3) show result
+    // function for creating short urls
+    const createAndInsert = (db, callback) => {
+      // first check if url is valid
       if (isValid(params)) {
+        // if valid: 1) generate id
         const shortId = generator();
         const doc = {url: params, short_id: shortId};
+        // 2) export to users url and short ID to DB in JSON
         collection.insert(doc, (err, result) => {
           if (err) console.log('Error inserting document into DB: ' + err);
           console.log('Inserted document into the collection');
         });
+        // 3) generate JSON result and pass to browser
         const showResult = {url: params, short_url: `https://urlit.herokuapp.com/${shortId}`};
         res.json(showResult);
       } else {
+        // if url is not valid, throw error
         res.json({error: 'Wrong url format'})
       }
-    }; // end of insert function
+    };
 
-    // search DB for existing urls, if not found, run insertDoc() and create
-    const searchdb = (db, callback) => {
+    // main function
+    const main = (db, callback) => {
+      // first check if users passed URL exists in DB, return result as short_id only
       collection.findOne({url: params}, {url: 0}, (err, doc) => {
         if (doc === null) {
-          insertDoc(db, () => db.close());
+          // if url not found in DB, run createAndInsert() function
+          createAndInsert(db, () => db.close());
         } else {
+          // if url exists, form JSON response with returned short_id and pass to browser
           const showResult = {url: params, short_url: `https://urlit.herokuapp.com/${doc.short_id}`};
           res.json(showResult);
         }
       });
-    }; // end of search in DB
-    // initiate function
-    searchdb(db, () => db.close());
-  }); // end connection to mongo
-}); // end of app GET
+    };
+    // initiate function, pass DB closing callback
+    main(db, () => db.close());
+  });
+});
 
 
-// redirect function
+// route for redirection
 app.get('/:shortid', (req, res) => {
+  // connect to MongoLab database server
   mongo.connect(mongoLabUrl, (err, db) => {
     if (err) console.log('Error connecting to DB server: ' + err);
     console.log('Connected to DB server');
 
     const collection = db.collection('links');
-    const params = req.params.shortid; // this will be short id
+    const params = req.params.shortid;
 
+    // check if user passed valid short url
     const findId = (db, callback) => {
+      // seach db by short_id, returning only url
       collection.findOne({short_id: params}, {url: 1}, (err, doc) => {
         if (doc === null) {
+          // if passed short_id not found in DB throw error
           res.json({error: 'Invalid short link'});
         } else {
-          // redirects to found url in DB
+          // if user passed valid id, then redirect
           res.redirect(doc.url);
         }
       });
     };
-    // initiate function
+    // initiate function, and pass db closing callback
     findId(db, () => db.close());
   });
-}); // end of app GET
+});
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
